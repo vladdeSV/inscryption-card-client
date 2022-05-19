6d5a2f9ab1c59fd2bd1ce27b537e93ae2ecab245
Vladimirs Nordholm <v@vladde.net>
2022-05-19 15:08:16 +0200

    minor restructure in select options

--- src/components/selectOptions.tsx
+++ src/components/selectOptions.tsx
@@ -23,16 +23,13 @@ type Props<T> = {
 
 export default class SelectOptions<T extends string> extends React.Component<Props<T>> {
   render() {
+    const option = (opt: Option<T>, index: number) => <option key={index} value={opt.value}>{opt.label}</option>
+
     return (
       <select name={this.props.uniqueName} disabled={this.props.disabled} onChange={e => this.props.onChange(e.target.value as T /* HACK! but will always be T*/)}>
-
         {this.props.groups
-          ? this.props.groups.map(group => (
-            <optgroup key={group.name} label={group.name}>
-              {group.options.map((opt, index) => <option key={index} value={opt.value}>{opt.label}</option>)}
-            </optgroup>
-          ))
-          : this.props.options.map((opt, index) => <option key={index} value={opt.value}>{opt.label}</option>)}
+          ? this.props.groups.map(group => <optgroup key={group.name} label={group.name}>{group.options.map(option)}</optgroup>)
+          : this.props.options.map(option)}
       </select>
     )
   }
