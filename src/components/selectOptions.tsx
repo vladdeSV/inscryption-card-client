da623655e22720fbf36aa2e64c701dcc20348c6d
Vladimirs Nordholm <v@vladde.net>
2022-05-19 15:10:38 +0200

    allow options to be disabled

--- src/components/selectOptions.tsx
+++ src/components/selectOptions.tsx
@@ -3,6 +3,7 @@ import React from "react";
 export type Option<T> = {
   value: T,
   label: string | JSX.Element,
+  enabled?: boolean,
 }
 
 type Foo<T> = {
@@ -23,7 +24,7 @@ type Props<T> = {
 
 export default class SelectOptions<T extends string> extends React.Component<Props<T>> {
   render() {
-    const option = (opt: Option<T>, index: number) => <option key={index} value={opt.value}>{opt.label}</option>
+    const option = (opt: Option<T>, index: number) => <option key={index} disabled={!(opt.enabled ?? true)} value={opt.value}>{opt.label}</option>
 
     return (
       <select name={this.props.uniqueName} disabled={this.props.disabled} onChange={e => this.props.onChange(e.target.value as T /* HACK! but will always be T*/)}>
