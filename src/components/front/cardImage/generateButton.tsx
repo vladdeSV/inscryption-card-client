ca4ef8ef89dd39b9dba1fe395a7f63181ce3785b
Vladimirs Nordholm <v@vladde.net>
2022-06-14 22:10:57 +0200

    patch: remove old unused duplicate code

--- src/components/front/cardImage/generateButton.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class GenerateButton extends React.Component {
-    render() {
-        return <span className="generate">+</span>
-    }
-}
--- src/components/special/cardImage/generateButton.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default class GenerateSpinner extends React.Component {
-    render() {
-        return <span className="generate">+</span>
-    }
-}
